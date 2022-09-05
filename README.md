4505766313a699eb172f9ebdde9393aecfd1994e
userdocs <16525024+userdocs@users.noreply.github.com>
2022-09-05 17:09:35 +0100

    build

---
+++ README.md
@@ -0,0 +1,18 @@
+
+```bash
+docker run -it -u 1000 -v ~/:/home/username ghcr.io/userdocs/autobrr-build:latest build.sh
+```
+
+Some very basic options
+
+*Note: using no flags will use the default `goreleaser` method and build multi arch targets in `autobrr/dist`*
+
+`-b` bootstrap by cloning the default repo and do nothing else.
+
+`-d` delete repo and re clone the default repo
+
+`-c` clear the repo and do nothing else.
+
+`-g` specify a github repo (fork) to clone
+
+`-m` build using `make` to quickly get a Linux binary for your arch in `autobrr/bin`
---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+github_url="https://github.com/autobrr/autobrr.git"
+
+while (("${#}")); do
+	case "${1}" in
+		-b)
+			bootstrap='yes'
+			;;&
+		-c)
+			rm -rf ~/autobrr
+			exit
+			;;
+		-g)
+			[[ -z "${2}" ]] && exit 1
+			github_url="${2}"
+			shift 2
+			;;&
+		-d)
+			rm -rf ~/autobrr
+			;;&
+		-m)
+			usemake="yes"
+			;;&
+		--) # end argument parsing
+			shift
+			break
+			;;
+		*) # preserve positional arguments
+			params1+=("${1}")
+			shift
+			;;
+	esac
+done
+
+set -- "${params1[@]}" # Set positional arguments in their proper place.
+
+if [[ ! -d ~/autobrr ]]; then
+	git clone "${github_url}" ~/autobrr
+else
+	echo "folder already exists"
+fi
+
+if [[ "${bootstrap}" != yes && -d ~/autobrr ]]; then
+	if [[ "${usemake}" == yes ]]; then
+		cd ~/autobrr || exit 1
+		make
+	else
+		cd ~/autobrr/web || exit 1
+		yarn install
+		yarn build
+		cd ~/autobrr || exit 1
+		goreleaser release --rm-dist --skip-publish --snapshot
+	fi
+fi
+exit
